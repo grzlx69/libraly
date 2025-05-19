38eaf8bfd4be97383e5ce62c81e9a8a3823242e3
grizlx69 <ivanovff12333@gmail.com>
2025-05-19 16:55:33 +0300

    Поправил проект, доработал недочёты.

--- render_website.py
+++ render_website.py
@@ -3,10 +3,11 @@ import os
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from more_itertools import chunked
 from livereload import Server
+import argparse
 
 
-def on_reload():
-    with open("books_params.json", "r", encoding="utf-8") as my_file:
+def on_reload(file_path):
+    with open(file_path, "r", encoding="utf-8") as my_file:
         books_params = json.load(my_file)
     env = Environment(
         loader=FileSystemLoader("."), autoescape=select_autoescape(["html", "xml"])
@@ -19,7 +20,7 @@ def on_reload():
 
     for number, books_page in enumerate(chunked_books):
 
-        index = env.get_template("./static/template.html")
+        index = env.get_template("template.html")
         rendered_page = index.render(
             books_params=books_page, page_number=number + 1, total_pages=total_pages
         )
@@ -28,8 +29,14 @@ def on_reload():
 
 
 def main():
+    parser = argparse.ArgumentParser(
+        description="Эта праграмма загружает данные из json файла, и публикует их на сайт - онлайн-библиотеку."
+    )
+    parser.add_argument("--path", help="Введите путь к файлу:", default="books_params.json")
+    args = parser.parse_args()
+    file_path = args.path
     server = Server()
-    server.watch("./static/template.html", on_reload())
+    server.watch("template.html", on_reload(file_path))
     server.serve(root=".", default_filename="./pages/index1.html")
 
 
